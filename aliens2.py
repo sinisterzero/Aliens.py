9f96273f424bbedd6ff5a06d3efaa1355d6d960a
csPython <cspython@AD25TF04NGF1J-85.local>
2018-12-04 11:11:25 -0500

    Added first while loop

--- aliens2.py
+++ aliens2.py
@@ -1,4 +1,4 @@
-#Added variables and fixed grammar and punctuation
+#Added first while loop
 
 week = str("week")
 lCheck = False
@@ -7,21 +7,25 @@ wCheck = False
 
 print ("You will first input the number of aliens that landed of Earth")
 print ("then input how many week have the aliens been on Earth for.")
-print ("Please input the number of aliens that have landed on Earth.")
-
-l = int(input())
-if l > 0:
-    print ("Please input how many weeks the aliens have been on Earth for.")            
+while lCheck==False:
+    try:
+        l = int(input("Please input the number of aliens that have landed on Earth. "))
+        if l > 0:
+            lCheck=True
+        else:
+            print("That is not a positive integer.")
+    except ValueError:
+        print("That is not a number.")
  
-    w = int(input())
-    if w > 0:
-        #calculations
-        for i in range(w):
-            if ( i == 1):
-                week = "weeks"
-            print("After", i + 1, week,", there are", l* 2**(i+1), "aliens on Earth.")
-    else:
-        print ("You didn't enter a valid answer.")
-        
+
+print ("Please input how many weeks the aliens have been on Earth for.")
+w = int(input())
+
+if w > 0:
+    #calculations
+    for i in range(w):
+        if ( i == 1):
+            week = "weeks"
+        print("After", i + 1, week,", there are", l* 2**(i+1), "aliens on Earth.")
 else:
     print ("You didn't enter a valid answer.")
